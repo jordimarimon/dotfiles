e9c80f7772cd1fd247fb3cfb09d62faf517ef50a
Jordi Marimon <jordimarimon7@gmail.com>
2023-12-08 13:43:24 +0100

    feat: show relative line numbers in neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -44,6 +44,9 @@ P.S. You can delete this when you're done too. It's your config now :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Set line numbers to be relative
+vim.wo.relativenumber = true
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
