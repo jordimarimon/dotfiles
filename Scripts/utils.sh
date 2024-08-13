26f9c3592de0779b9b6dc2cafbb255425733d6c8
Jordi Marimon <jordimarimon7@gmail.com>
2024-08-13 07:01:36 +0200

    refactor: remove unused bash functions

--- Scripts/utils.sh
+++ Scripts/utils.sh
@@ -1,34 +1,6 @@
 #!/bin/bash
 
 
-# FIXME: Migrate to wayland
-function open_pdf() {
-	window_id=$(xdo id);
-	file=$(printf '%q' "`find -type f -name \"*.pdf\" | fzf`");
-
-	if [[ "$file" != *.pdf ]]; then
-		exit 0;
-	fi
-
-	xdo hide $window_id;
-	bash -i -c "zathura $file > /dev/null 2>&1; exit 0";
-	xdo show $window_id;
-}
-
-# FIXME: Migrate to wayland
-function open_xlsx() {
-	window_id=$(xdo id);
-	file=$(printf '%q' "`find -type f -name \"*.xlsx\" | fzf`");
-
-	if [[ "$file" != *.xlsx ]]; then
-		exit 0;
-	fi
-
-	xdo hide $window_id;
-	bash -i -c "libreoffice --calc $file > /dev/null 2>&1; exit 0";
-	xdo show $window_id;
-}
-
 function open_notes() {
 	notes=($(ls "$HOME/Notes/"));
 
