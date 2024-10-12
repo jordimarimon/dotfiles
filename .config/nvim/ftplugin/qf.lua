d232264da50fa36a9d4bb0c4206d864814796a12
Jordi Marimon <jordimarimon7@gmail.com>
2024-10-12 10:12:44 +0200

    feat: add keymaps for cnext and cprev

--- .config/nvim/ftplugin/qf.lua
+++ .config/nvim/ftplugin/qf.lua
@@ -2,3 +2,17 @@
 vim.keymap.set("n", "dd", require("custom.quickfix").rm_qf_item, { desc = "Remove QF entry", buffer = true, silent = true })
 vim.keymap.set("v", "d", require("custom.quickfix").rm_qf_items, { desc = "Remove QF entries", buffer = true, silent = true })
 
+-- Be able to move between entries
+local function move_to_next_qf()
+    vim.cmd("cnext")
+    vim.cmd("copen")
+end
+
+local function move_to_prev_qf()
+    vim.cmd("cprev")
+    vim.cmd("copen")
+end
+
+vim.keymap.set("n", "<M-j>", move_to_next_qf, { desc = "Move to next QF entry", buffer = true, silent = true })
+vim.keymap.set("n", "<M-k>", move_to_prev_qf, { desc = "Move to previous QF entry", buffer = true, silent = true })
+
