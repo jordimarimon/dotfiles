7ca6d2a2a8051a85311dcaac0e20bd544f8cce98
Jordi Marimon <jordimarimon7@gmail.com>
2024-10-19 10:49:17 +0200

    refactor: disable treesitter highlighting in large files

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -27,6 +27,13 @@ return {
 			ignore_install = {},
 			highlight = {
 				enable = true,
+				disable = function (lang, buf)
+					local max_filesize = 100 * 1024 -- 100 KB
+					local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+				end,
 			},
 			match = {
 				enable = true,
