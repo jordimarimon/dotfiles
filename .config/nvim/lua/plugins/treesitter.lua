757b9b6673158ae1843e95a7fd4f612a1208729b
Jordi Marimon <jordimarimon7@gmail.com>
2024-11-08 08:59:06 +0100

    feat: add treesj to split/join code blocks

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,91 +4,106 @@
 --	- Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
 --	- Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	main = "nvim-treesitter.configs",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-	},
-	config = function ()
-		local configs = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		main = "nvim-treesitter.configs",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		config = function ()
+			local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = {
-				"c", "cpp", "dockerfile", "lua", "vim", "vimdoc", "query",
-				"javascript", "typescript", "php", "html",
-				"css", "angular", "bash", "json",
-				"sql", "tsx", "yaml", "python", "editorconfig", "make",
-				"markdown", "markdown_inline"
-			},
-			auto_install = true,
-			sync_install = false,
-			modules = {},
-			ignore_install = {},
-			highlight = {
-				enable = true,
-				disable = function (lang, buf)
-					local max_filesize = 500 * 1024 -- 500 KB
-					local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-					if ok and stats and stats.size > max_filesize then
-						return true
-					end
-				end,
-			},
-			match = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<Leader>ss",
-					node_incremental = "<Leader>si",
-					scope_incremental = "<Leader>sc",
-					node_decremental = "<Leader>sd",
+			configs.setup({
+				ensure_installed = {
+					"c", "cpp", "dockerfile", "lua", "vim", "vimdoc", "query",
+					"javascript", "typescript", "php", "html",
+					"css", "angular", "bash", "json",
+					"sql", "tsx", "yaml", "python", "editorconfig", "make",
+					"markdown", "markdown_inline"
 				},
-			},
-			textobjects = {
-				swap = {
+				auto_install = true,
+				sync_install = false,
+				modules = {},
+				ignore_install = {},
+				highlight = {
 					enable = true,
-					swap_next = { ["<Leader>>"] = "@parameter.inner" },
-					swap_previous = { ["<Leader><"] = "@parameter.inner" },
+					disable = function (_, buf)
+						local max_filesize = 500 * 1024 -- 500 KB
+						local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							return true
+						end
+					end,
 				},
-				select = {
+				match = {
 					enable = true,
-					lookahead = true,
+				},
+				indent = {
+					enable = true,
+				},
+				incremental_selection = {
+					enable = true,
 					keymaps = {
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
+						init_selection = "<Leader>ss",
+						node_incremental = "<Leader>si",
+						scope_incremental = "<Leader>sc",
+						node_decremental = "<Leader>sd",
 					},
 				},
-				move = {
-					enable = true,
-					set_jumps = true,
-					goto_next_start = {
-						["]]"] = "@jsx.element",
-						["]f"] = "@function.outer",
-						["]m"] = "@class.outer",
+				textobjects = {
+					swap = {
+						enable = true,
+						swap_next = { ["<Leader>>"] = "@parameter.inner" },
+						swap_previous = { ["<Leader><"] = "@parameter.inner" },
 					},
-					goto_next_end = {
-						["]F"] = "@function.outer",
-						["]M"] = "@class.outer",
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
 					},
-					goto_previous_start = {
-						["[["] = "@jsx.element",
-						["[f"] = "@function.outer",
-						["[m"] = "@class.outer",
-					},
-					goto_previous_end = {
-						["[F"] = "@function.outer",
-						["[M"] = "@class.outer",
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]]"] = "@jsx.element",
+							["]f"] = "@function.outer",
+							["]m"] = "@class.outer",
+						},
+						goto_next_end = {
+							["]F"] = "@function.outer",
+							["]M"] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[["] = "@jsx.element",
+							["[f"] = "@function.outer",
+							["[m"] = "@class.outer",
+						},
+						goto_previous_end = {
+							["[F"] = "@function.outer",
+							["[M"] = "@class.outer",
+						},
 					},
 				},
-			},
-		})
-	end,
+			})
+		end,
+	},
+	{
+		"Wansmer/treesj",
+		keys = { "<leader>ts", "<leader>tj" },
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("treesj").setup({
+				use_default_keympas = false,
+			})
+
+			vim.keymap.set("n", "<leader>ts", function() require("treesj").split() end, { desc = "[T]reesitter [S]plit" })
+			vim.keymap.set("n", "<leader>tj", function() require("treesj").join() end, { desc = "[T]reesitter [J]oin" })
+		end,
+	}
 }
