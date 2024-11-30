7e0390f173648ef4057d6e89fae64c5f532c101d
Jordi Marimon <jordimarimon7@gmail.com>
2024-11-30 19:19:56 +0100

    refactor: remove vscode snippets

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -11,10 +11,6 @@ return {
 				"rafamadriz/friendly-snippets",
 				config = function()
 					require("luasnip.loaders.from_vscode").lazy_load({
-						exclude = { "markdown" }
-					})
-
-					require("luasnip.loaders.from_vscode").lazy_load({
 						paths = {
 							"~/.config/nvim/lua/snippets/",
 						},
