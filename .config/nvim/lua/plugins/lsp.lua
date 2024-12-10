81313221384366000a0d5b19103c315502cfb3e6
Jordi Marimon <jordimarimon7@gmail.com>
2024-12-10 07:45:14 +0100

    refactor: use default keybindings for LSP

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -46,40 +46,32 @@ return {
 						return
 					end
 
-					-- Jump to the definition of the word under your cursor.
-					-- This is where a variable was first declared, or where a function is defined, etc.
-					-- To jump back, press <C-t>.
-					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-
 					-- Find references for the word under your cursor.
-					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+					map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
 					--- Displays hover information about the symbol under the cursor in a floating window
 					map("K", function() vim.lsp.buf.hover({border = "single"}) end, "Show symbol info")
 
 					-- Jump to the implementation of the word under your cursor.
 					-- Useful when your language has ways of declaring types without an actual implementation.
-					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+					map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+
+					-- Fuzzy find all the symbols in your current document.
+					-- Symbols are things like variables, functions, types, etc.
+					map("g0", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+
+					-- Jump to the definition of the word under your cursor.
+					-- This is where a variable was first declared, or where a function is defined, etc.
+					-- To jump back, press <C-t>.
+					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+
+					-- This is not Goto Definition, this is Goto Declaration.
+					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 					-- Jump to the type of the word under your cursor.
 					-- Useful when you"re not sure what type a variable is and you want to see
 					-- the definition of its *type*, not where it was *defined*.
 					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-
-					-- Fuzzy find all the symbols in your current document.
-					-- Symbols are things like variables, functions, types, etc.
-					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-
-					-- Fuzzy find all the symbols in your current workspace.
-					-- Similar to document symbols, except searches over your entire project.
-					map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-					-- Execute a code action, usually your cursor needs to be on top of an error
-					-- or a suggestion from your LSP for this to activate.
-					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
-
-					-- This is not Goto Definition, this is Goto Declaration.
-					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 					-- Format file
 					if client:supports_method(vim.lsp.protocol.Methods.textDocument_formatting) then
