1f2d2e705291c772cb5d5fd80e8db255616424f4
Jordi Marimon <jordimarimon7@gmail.com>
2025-01-04 09:21:17 +0100

    refactor: update nvim plugins

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -58,7 +58,7 @@ vim.o.timeoutlen = 500
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = "split"
