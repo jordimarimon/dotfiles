28f95cf03853dfc3b106258defaa84e20109da6d
Jordi Marimon <jordimarimon7@gmail.com>
2025-01-12 07:45:25 +0100

    refactor: remove unused project from tmux sessions

--- Scripts/tmux-create-sessions.sh
+++ Scripts/tmux-create-sessions.sh
@@ -5,9 +5,8 @@
 declare -A PATHS
 PATHS[0]="$HOME/Interactiu/erp-frontend"
 PATHS[1]="$HOME/Interactiu/erp-web"
-PATHS[2]="$HOME/Interactiu/interactiu-web-components"
-PATHS[3]="$HOME/Interactiu/erp-import-app"
-PATHS[4]="$HOME/Interactiu/erp-console-api"
+PATHS[2]="$HOME/Interactiu/erp-import-app"
+PATHS[3]="$HOME/Interactiu/erp-console-api"
 
 # Loop through each session and creates it
 for INDEX in "${!PATHS[@]}"; do
@@ -15,7 +14,7 @@ for INDEX in "${!PATHS[@]}"; do
     SESSION_NAME=$(basename "$SESSION_PATH" | tr . _)
 
     # Check if the session already exists
-    if tmux has-session -t $SESSION_NAME 2>/dev/null; then
+    if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
 	continue
     fi
 
