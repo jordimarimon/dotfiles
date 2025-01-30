34d7920791cbc801e598793d5cfac36117fbd9dd
Jordi Marimon <jordimarimon7@gmail.com>
2025-01-30 07:51:21 +0100

    refactor: update plugins

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -5,7 +5,15 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local todo_comments = require("todo-comments")
-        todo_comments.setup({ signs = false })
+        todo_comments.setup({
+            signs = false,
+            highlight = {
+                pattern = [[(KEYWORDS)\s*(\([^\)]*\))?:]],
+            },
+            search = {
+                pattern = [[(KEYWORDS)\s*(\([^\)]*\))?:]],
+            },
+        })
 
         vim.keymap.set("n", "<leader>st", vim.cmd.TodoTelescope, { desc = "[S]earch [T]odos", silent = true })
         vim.keymap.set("n", "]t", function() todo_comments.jump_next() end, { desc = "Next todo comment", silent = true })
