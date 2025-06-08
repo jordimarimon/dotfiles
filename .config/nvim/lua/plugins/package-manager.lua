4c45e77e391fe6eac8e55b1a7f9672f8662e2cfb
Jordi Marimon <jordimarimon7@gmail.com>
2025-06-08 18:21:19 +0200

    refactor: automate installation of LSPs and formatters

--- .config/nvim/lua/plugins/package-manager.lua
+++ .config/nvim/lua/plugins/package-manager.lua
@@ -2,7 +2,42 @@
 return {
     "williamboman/mason.nvim",
     config = function()
+        local registry = require("mason-registry")
+        local packages = {
+            "basedpyright",
+            "bash-language-server",
+            "clang-format",
+            "clangd",
+            "css-lsp",
+            "css-variables-language-server",
+            "goimports",
+            "gopls",
+            "html-lsp",
+            "intelephense",
+            "json-lsp",
+            "lua-language-server",
+            "typescript-language-server",
+        }
+
         ---@diagnostic disable-next-line: missing-fields
         require("mason").setup({ ui = { border = "single" } })
+
+        registry.refresh(function (success)
+            if not success then
+                return
+            end
+
+            local packages_to_install = {}
+
+            for _, pkg in ipairs(packages) do
+                if not registry.is_installed(pkg) then
+                    table.insert(packages_to_install, pkg)
+                end
+            end
+
+            if #packages_to_install > 0 then
+                vim.cmd("MasonInstall " .. table.concat(packages_to_install, " "))
+            end
+        end)
     end,
 }
